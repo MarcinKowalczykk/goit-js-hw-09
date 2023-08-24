846ed47bea4446461959a693a08e39d22ac5469c
Marcin Kowalczyk <marcinck80@gmail.com>
2023-08-24 20:31:37 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,21 @@
 function getRandomHexColor() {
-  return;
-  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let intervalId = null;
+
+document.querySelector('[data-start]').addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+
+  document.querySelector('[data-start]').disabled = true;
+  document.querySelector('[data-stop]').disabled = false;
+});
+
+document.querySelector('[data-stop]').addEventListener('click', () => {
+  clearInterval(intervalId);
+  document.querySelector('[data-start]').disabled = false;
+  document.querySelector('[data-stop]').disabled = true;
+});
